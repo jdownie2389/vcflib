857cdc5cb6588e618f9b70ceecf97f9f4d148602
Jonathan Downie <jonathan.downie@hsc.utah.edu>
2014-07-04 09:58:00 -0600

    Initial commit from local git

---
+++ archaicsearch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  archaicsearch
+//
+//  Created by Jonathan Downie on 7/4/14.
+//  Copyright (c) 2014 Jonathan Downie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
